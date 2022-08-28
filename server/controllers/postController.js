837409ac16033f2d8dfc8ed9a30328e4c996d074
bleclub <bleclub_5@live.com>
2022-08-28 11:52:57 +0700

    create new vue app with vue cli

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -6,7 +6,7 @@ module.exports = class postController {
   static async fetchAllPost(req, res) {
     try {
       const posts = await Post.find();
-      res.status(200).json(posts);
+      res.status(200).json({ posts });
     } catch (error) {
       res.status(404).json({ message: error.message });
     }
